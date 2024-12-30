9c095fee2e4426596e76388f902ce8172f0ab951
AkAsH240904 <146891643+AkAsH240904@users.noreply.github.com>
2024-12-31 01:20:50 +0530

    Add files via upload

---
+++ yolo image detection/Home.py
@@ -0,0 +1,31 @@
+import streamlit as st 
+
+st.set_page_config(page_title="Home",
+                   layout='wide',
+                   page_icon='./images/home.png')
+
+st.title("YOLO V5 Object Detection App")
+st.caption('This web application demostrate Object Detection')
+
+# Content
+st.markdown("""
+### This App detects objects from Images
+- Automatically detects 20 objects from image
+- [Click here for App](/YOLO_for_image/)  
+
+Below give are the object the our model will detect
+1. Person
+2. Car
+3. Chair
+4. Bottle
+5. Sofa
+6. Bicyle
+7. Horse
+8. Boat
+9. Motor Bike
+10. Tv Monitor
+11. Aeroplane
+12. Train
+13. Bus
+    
+            """)
